a44c1359ba572de971f22a5c29faed0a4f25e61e
Martin Ohlin <Martin.p.ohlin@@gmail.com>
2014-10-11 23:17:44 +0200

    Trying out Parse.

---
+++ app/src/main/java/com/martin/vaxjobicycleguide/BikeApplication.java
@@ -0,0 +1,14 @@
+package com.martin.vaxjobicycleguide;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class BikeApplication extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "5CRPyrJBVJACJSfINFZq741rNsCXnZbxXiVjT8EB", "sydCG04vI4TpGW2qvL7yeNUwJHLFlzfd6cpCVp5g");
+    }
+}
--- app/src/main/java/com/martin/vaxjobicycleguide/RouteListFragment.java
+++ app/src/main/java/com/martin/vaxjobicycleguide/RouteListFragment.java
@@ -2,14 +2,39 @@ package com.martin.vaxjobicycleguide;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
 
 public class RouteListFragment extends ListFragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Route");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> parseObjects, ParseException e) {
+                if (e == null) {
+                    for (ParseObject route: parseObjects) {
+                        Log.d("Växjö Bicycle Guide", route.getString("Name"));
+                    }
+                } else {
+                    // something went wrong
+                    Log.d("Växjö Bicycle Guide", e.toString());
+                }
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_route_list, container, false);
     }
 }
